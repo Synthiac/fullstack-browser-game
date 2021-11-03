9ee1118d1f28f980cd2320d79e4b4c6adcbfe03f
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-03 13:07:21 -0700

    moved preload, created json mapping and embeded tileset

--- game.js
+++ game.js
@@ -1,34 +1,30 @@
 // make sure to pull form single object for level design
 // break up character sprites for use, coins plus eniemes
+// can add scene model for levels in tiled
 
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    parent: 'phaser-example',
+    parent: 'canvas',
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 500 },
             debug: false
         }
     },
     scene: {
+        key: "main",
         preload: preload,
         create: create,
         update: update
     }
 };
 
-var player;
-var bolt;
-var platforms;
-var cursors;
-var movingPlatform;
-
-var game = new Phaser.Game(config);
-
 function preload() {
+    this.load.image('tile', '/assests/map/mytileset.json')
+    this.load.tilemapTiledJSON('map', '/assets/map/map.json');
     this.load.image('sky', '/assets/background3.png');
     this.load.image('ground', '/assets/platform.png');
     this.load.image('bolt', '/assets/Bolt.png');
@@ -37,35 +33,52 @@ function preload() {
     this.load.image('cloud2', '/assets/cloud7.png');
     this.load.image('cloud3', '/assets/cloud6.png');
     this.load.image('cloud4', '/assets/cloud5.png');
-    this.load.image('lightning', '/assets/lighting.png');
+    this.load.image('lightning', '/assets/lighting.png'); 
     this.load.audio("theme", "/assets/theme.mp3");
 }
 
+var game = new Phaser.Game(config);
+var player;
+var bolt;
+var platforms;
+var cursors;
+var movingPlatform;
+var map;
+var groundLayer;
+var tileSet
+
+
+
+
 function create() {
-    this.add.image(400, 300, 'sky');
-    this.add.image(600, 250, 'cloud1');
-    this.add.image(700, 150, 'cloud2');
-    this.add.image(750, 400, 'cloud3');
-    this.add.image(300, 450, 'cloud4');
+    map = this.make.tilemap({ key: "map" });
+    var tileSet = map.addTilestImage("tileset", "tiles");
+    var groundLayer = map.createStaticLayer('Tiles', tileSet, 0, 0);
 
-    platforms = this.physics.add.staticGroup();
-    clouds = this.physics.add.staticGroup();
+    // this.add.image(400, 300, 'sky');
+    // this.add.image(600, 250, 'cloud1');
+    // this.add.image(700, 150, 'cloud2');
+    // this.add.image(750, 400, 'cloud3');
+    // this.add.image(300, 450, 'cloud4');
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    // platforms = this.physics.add.staticGroup();
+    // clouds = this.physics.add.staticGroup();
+
+    // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
     
 
 
-    movingPlatform = this.physics.add.image(400, 400, 'ground');
-    movingPlatform.setImmovable(true);
-    movingPlatform.body.allowGravity = false;
-    movingPlatform.setVelocityX(50);
+    // movingPlatform = this.physics.add.image(400, 400, 'ground');
+    // movingPlatform.setImmovable(true);
+    // movingPlatform.body.allowGravity = false;
+    // movingPlatform.setVelocityX(50);
 
-    clouds.create(500, 668, 'cloud1').setScale(2).refreshBody();
+    // clouds.create(500, 668, 'cloud1').setScale(2).refreshBody();
     
-    movingClouds = this.physics.add.image(500, 250, 'cloud1');
-    movingClouds.setImmovable(true);
-    movingClouds.body.allowGravity = false;
-    movingClouds.setVelocityX(50);
+    // movingClouds = this.physics.add.image(500, 250, 'cloud1');
+    // movingClouds.setImmovable(true);
+    // movingClouds.body.allowGravity = false;
+    // movingClouds.setVelocityX(50);
     
 
     player = this.physics.add.sprite(80, 400, 'king');
@@ -113,11 +126,11 @@ function create() {
 
     });
 
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(player, movingPlatform);
-    this.physics.add.collider(bolt, platforms);
-    this.physics.add.collider(bolt, movingPlatform);
-    this.physics.add.collider(bolt, movingPlatform);
+    // this.physics.add.collider(player, platforms);
+    // this.physics.add.collider(player, movingPlatform);
+    // this.physics.add.collider(bolt, platforms);
+    // this.physics.add.collider(bolt, movingPlatform);
+    // this.physics.add.collider(bolt, movingPlatform);
 
     this.physics.add.overlap(player, bolt, collectBolt, null, this);
     theme = this.sound.add("theme");
@@ -161,25 +174,20 @@ function update() {
         movingPlatform.setVelocityX(50);
     }
 }
-function cloudMovement(){
-    if (movingCloud.x >= 500) {
-        movingCloud.setVelocityX(-50);
-    }
-    else if (movingCloud.x <= 300) {
-        movingCloud.setVelocityX(50);
-    }
-}
+// function cloudMovement(){
+//     if (movingCloud.x >= 500) {
+//         movingCloud.setVelocityX(-50);
+//     }
+//     else if (movingCloud.x <= 300) {
+//         movingCloud.setVelocityX(50);
+//     }
+// }
 
 //action of picking up bolt, session or db relationship
-function collectBolt(player, bolt) {
-    console.log("points up")
-    bolt.disableBody(true, true);
-=======
-
-function collectStar(player, star) {
-    console.log("duck")
-    star.disableBody(true, true);
-
+// function collectBolt(player, bolt) {
+//     console.log("points up")
+//     bolt.disableBody(true, true);
+// };
 
     // This is the route that gets our comments
     // corresponds to in game event trigger
@@ -189,4 +197,3 @@ function collectStar(player, star) {
     //     })
     // }
 
-}
